249a73b42bd233e71408e7181cbda10331ba7034
刘文凯 <mister_akai@126.com>
2018-08-31 17:13:21 +0800

    [DEV]项目测试资源提交

--- assets/script/Helloworld.ts
+++ assets/script/Helloworld.ts
@@ -1,5 +1,6 @@
 import BroadcastManager from "./base/BroadcastManager";
 import AudioManager from "./base/AudioMananger";
+import SocketManager from "./base/SocketManager";
 
 const { ccclass, property } = cc._decorator;
 
@@ -16,6 +17,7 @@ export default class Helloworld2 extends cc.Component {
 
     private _broadcastManager: BroadcastManager;
     private _audioManager: AudioManager;
+    _socket: SocketManager;
 
     onLoad() {
         this._broadcastManager = this.node.getComponent(BroadcastManager);
@@ -25,16 +27,27 @@ export default class Helloworld2 extends cc.Component {
 
     start() {
         // init logic
-
+        this._socket = SocketManager.getInstance(null);
         this._audioManager.playMusic("music_logo.mp3");
-
         this.node.on("test", function () {
             this.label.string = this.t++;
         }.bind(this))
-
         this.node.runAction(cc.repeatForever(cc.sequence([cc.delayTime(0.02), cc.callFunc(function () {
-            this._broadcastManager.sendBroadcast("SAY_HELLO_1", "Hello " + this.t++);
+            // this._broadcastManager.sendBroadcast("SAY_HELLO_1", "Hello " + this.t++);
             // this.node.emit("test")
+            // this._socket.send("" + this.t++)
+            // console.log(this.t)
+            if (this.t++ == 20) {
+                this._socket.send("4")
+            }
+
+            if (this.t == 100) {
+                this._socket.close();
+            }
+
+            if (this.t == 200) {
+                this._socket.open();
+            }
         }.bind(this))])))
     }
 
