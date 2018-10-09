ef7cae5996d19f92eb52d3e833eb182e6d12f1aa
刘文凯 <mister_akai@126.com>
2018-10-09 14:39:25 +0800

    [DEV]测试脚本更新

--- assets/script/Helloworld.ts
+++ assets/script/Helloworld.ts
@@ -1,11 +1,15 @@
 import Base from "./base/Base";
 import { HttpResponse } from "./base/network/HttpManager";
 import BroadcastComponent from "./base/components/BroadcastComponent";
+import GaussBlurs from "./base/shader/shaders/GaussBlurs";
+import OverlayShader from "./base/shader/shaders/OverlayShader";
+import RainShader from "./base/shader/shaders/RainShader";
+import WaveShader from "./base/shader/shaders/WaveShader";
 
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class Helloworld2 extends cc.Component {
+export default class Helloworld extends cc.Component {
 
     @property(cc.Label)
     label: cc.Label = null;
@@ -13,9 +17,13 @@ export default class Helloworld2 extends cc.Component {
     @property
     text: string = 'hello';
 
+    @property(cc.Sprite)
+    sprite: any = null;
+
     t = 0;
 
     private _broadcastManager: BroadcastComponent;
+    startTime: number;
 
     onLoad() {
         this._broadcastManager = this.node.getComponent(BroadcastComponent);
@@ -62,5 +70,34 @@ export default class Helloworld2 extends cc.Component {
 
     SAY_HELLO_2(data) {
         this.label.string = data;
+    }
+
+    GaussBlurs() {
+        this.startTime = new Date().getTime();
+        Base.ShaderManager.setShader(this.sprite, "FluxaySuper").setParamValue('bluramount', 0.1);
+    }
+    overlay() {
+        this.startTime = new Date().getTime();
+        Base.ShaderManager.setShader(this.sprite, "OverlayShader");
+    }
+    rainheart() {
+        this.startTime = new Date().getTime();
+        let mat = Base.ShaderManager.setShader(this.sprite, "RainShader");
+    }
+    wave() {
+        this.startTime = new Date().getTime();
+        Base.ShaderManager.setShader(this.sprite, "WaveShader").setParamValue('iOffset', new cc.Vec2(0, 0.1));
+    }
+
+    update(dt) {
+        let mat = this.sprite.getCurrMaterial();
+        if (!mat) {
+            return;
+        }
+        if (["RainShader", "WaveShader", "FluxaySuper"].indexOf(mat.name) > -1) {
+            let now = new Date().getTime();
+            let time = (now - this.startTime) / 1000;
+            mat.setParamValue('time', time);
+        }
     }
 }
