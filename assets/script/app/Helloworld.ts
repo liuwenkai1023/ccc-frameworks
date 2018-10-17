f33b50b5284e8dbba58fbeace73b9e8f5d15378a
刘文凯 <mister_akai@126.com>
2018-10-17 17:53:15 +0800

    [DEV]组件常规更新

--- assets/script/app/Helloworld.ts
+++ assets/script/app/Helloworld.ts
@@ -1,11 +1,12 @@
-import Base from "../base/Base";
 import BaseComponent from "../base/BaseComponent";
+import Base from "../base/Base";
 import { HttpResponse } from "../base/network/HttpManager";
+
 
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class Helloworld extends BaseComponent {
+export default class HelloWorld extends BaseComponent {
 
     @property(cc.Label)
     label: cc.Label = null;
@@ -21,30 +22,21 @@ export default class Helloworld extends BaseComponent {
     onLoaded() {
         this.initReceiver();
         this.time = new Date().getTime();
-        Base.Audio.playMusic("ding.wav");
+        Base.AudioManager.playMusic("ding.wav");
         Base.SocketManager.open();
-        // FileUtils.instance().getDataFromFile(FileUtils.writeablePath + 'resources/json/data.json').then(data => {
-        //     console.log("data =" + data);
-        // }).catch(err => {
-        //     console.error(err);
-        // });
-        // FileUtils.instance().saveFile("122222222222222222222222222222222222222", FileUtils.writeablePath + 'resources/json/data')
-        // FileUtils.instance().renameFile(FileUtils.writeablePath + 'resources/json/data.json', FileUtils.writeablePath + 'resources/json/data')
-        // console.error("创建" + FileUtils.instance().removeDirectory(FileUtils.writeablePath + 'resources/12313/123123122/', true))
     }
 
 
     start() {
         this.node.runAction(cc.repeatForever(cc.sequence([cc.delayTime(0.01), cc.callFunc(function () {
             this.broadcastManager.sendBroadcast("SAY_HELLO_1", this.t++)
+            console.log(this.broadcastManager)
             Base.SocketManager.send("" + this.t++);
         }.bind(this))])))
 
-        Base.Http.HTTP_GET("", null, function (response: HttpResponse) {
+        Base.HttpManager.HTTP_GET("", null, function (response: HttpResponse) {
             this.label.string = response.event;
         }.bind(this));
-
-        Base.Http.HTTP_POST("", null, null);
 
         let encode = Base.Base64.encode("测试Base64▲▼●◆■āáǎà");
         let decode = Base.Base64.decode(encode);
@@ -67,14 +59,12 @@ export default class Helloworld extends BaseComponent {
     SAY_HELLO_1(data) {
         this.label.string = data;
         Base.SocketManager.send(data);
-        // console.log(data, new Date().getTime() - this.time);
         this.time = new Date().getTime();
     }
 
 
     SAY_HELLO_2(data) {
         this.label.string = data;
-        // console.log(data, new Date().getTime() - this.time);
         this.time = new Date().getTime();
     }
 
